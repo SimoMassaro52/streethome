3e5e8ecf6132c81605abb052f7526113c3133f74
SimoMassaro52 <simonesimomassaro@gmail.com>
2023-09-06 16:36:17 +0200

    testi contest aggiornati

--- src/pages/DayC.jsx
+++ src/pages/DayC.jsx
@@ -8,7 +8,219 @@ export default function DayC() {
 				<div className="img-wrap">
 					<img src={dayC} />
 					<div className="text-wrap">
-						Officia Lorem duis pariatur irure minim pariatur irure ad nulla ea.
+						<p className="day-title">REGOLAMENTO</p>
+						<br />
+						<p className="day-txt-bold">SURF MECCANICO</p>
+						<p className="contest-txt">
+							Contest a iscrizione gratuita - unico obbligo essere follower
+							delle pagina instagram
+							<span className="day-txt-bolder"> “HOME **CK BAR”</span>
+						</p>
+						<p className="contest-txt">
+							Ogni iscritto può effettuare al massimo
+							<span className="day-txt-bolder"> 3 tentativi</span>
+						</p>
+						<p className="contest-txt">
+							Il vincitore sarà decretato tra tutti i partecipanti iscritti ogni
+							giorno dall’apertura dell’area fino alle 22:00, sarà colui il
+							quale è rimasto più tempo in equilibrio senza cadere dalla tavola
+							da surf.
+						</p>
+						<p className="contest-txt">
+							Il tempo minimo considerato sarà di
+							<span className="day-txt-bolder"> 90 secondi</span>.
+						</p>
+						<p className="contest-txt">
+							Ogni qualvolta sarà superato il tempo record giornaliero si avrà
+							diritto ad <span className="day-txt-bolder">una birra</span>
+						</p>
+						<p className="contest-txt">
+							Come premio al vincitore, vi è un buono di 50€ al giorno
+						</p>
+						<br />
+						<p className="day-txt-bold">SKATE TRICK</p>
+						<p className="day-txt-reg">
+							Contest powered by
+							<span className="day-txt-bolder"> THE SHIFTY CREW</span>
+						</p>
+						<p className="day-txt-bolder">“play a good trick for a prize” </p>
+						<p className="contest-txt">
+							I vincitori vinceranno
+							<span className="day-txt-bolder">
+								premi by Lobster e HOME **CK BAR
+							</span>
+						</p>
+						<p className="contest-txt">
+							Iscrizione <span className="day-txt-bolder">gratuita</span> in
+							loco.
+						</p>
+						<br />
+						<p className="day-txt-bold">Basket 2 vs 2 misto</p>
+						<p className="contest-txt">
+							• Il torneo si terrà su asfalto e su un campo non regolamentare ma
+							<span className="day-txt-bolder"> “street”</span>. Il torneo non è
+							agonistico.
+						</p>
+						<p className="contest-txt">
+							• L’unica regola regina è la volontà di divertirsi e{" "}
+							<span className="day-txt-bolder">il fair play</span>. Ogni
+							incontro sarà controllato da un “giudice di campo” (segnapunti e
+							cronometrista), il quale decide sui falli o le infrazioni e, nel
+							caso di conclamate proteste, o comportamenti antisportivi,
+							irrispettosi e maleducati, ha anche il potere di espellere
+							giocatori e/o sospendere una partita. La sospensione per diverbi
+							e/o rissa tra le due squadre determina la squalifica automatica
+							delle due squadre.
+						</p>
+						<p className="contest-txt">
+							• Le bestemmie sono accettate solo se simpatiche e non siano
+							offensive verso le scelte personali circa la religione, la
+							politica o indirizzo sessuali.
+						</p>
+						<p className="contest-txt">
+							• Una squadra è composta da 3 giocatori 2 in campo più 1 in
+							panchina.
+						</p>
+						<p className="contest-txt">
+							• Ogni squadra dovrà presentarsi all’organizzazione{" "}
+							<span className="day-txt-bolder">
+								5 minuti prima dell’inizio della partita
+							</span>
+							.
+						</p>
+						<p className="contest-txt">
+							• Le partite possono iniziare solo con la presenza di
+							<span className="day-txt-bolder"> minimo 2 </span>
+							giocatori per squadra.
+						</p>
+						<p className="contest-txt">
+							• Una partita si conclude non appena una squadra raggiunge o
+							supera i 21 punti, entro comunque un tempo massimo di 15 minuti di
+							gioco (tempo reale). Se alla conclusione del 15° minuto di gioco
+							nessuna squadra ha raggiunto o superato i{" "}
+							<span className="day-txt-bolder"> 21 punti </span>, si aggiudica
+							la partita la squadra in vantaggio in quel momento. In caso di
+							parità si disputano tempi supplementari di 1 minuto (uno o più
+							tempi a seconda della necessità) sino a che non si spezza la
+							situazione di parità.
+						</p>
+						<p className="contest-txt">
+							• La partita ha inizio con un possesso palla alla squadra che
+							vince un “pari e dispari”. Dopodiché, in caso di “palla contesa”,
+							il possesso palla (rimessa in gioco) si alterna.
+						</p>
+						<p className="contest-txt">
+							• Per i falli e le infrazioni di gioco valgono (in linea di
+							massima) le <span className="day-txt-bolder"> regole FIBA </span>,
+							con le seguenti principali eccezioni: dopo un canestro segnato su
+							azione (da 2 o d a 3 punti) la palla resta alla squadra che ha
+							segnato il canestro (in attacco), con l’obbligo di tornare
+							indietro all’inizio del campo per iniziare una nuova azione in
+							attacco. L’attaccante che inizia l’azione di gioco ha l’obbligo di
+							effettuare almeno un passaggio al compagno (non può cioè tirare o
+							andare direttamente a canestro); se la difesa cattura un rimbalzo
+							o intercetta una palla, per andare a canestro deve uscire dalla
+							linea dove sono gli attaccanti, non esiste il limite di falli e
+							non si assegnano tiri liberi, ogni tre falli di squadra viene
+							assegnato un punto agli avversari più il possesso di palla; non
+							esiste l a regola dei 3” (in area) e dei 24” (in attacco); non son
+							o consentiti time-out e/o interruzioni di gioco, se non nel caso
+							di evidenti necessità (infortuni, maltempo, incidenti
+							organizzativi...);
+						</p>
+						<p className="contest-txt">
+							• le sostituzioni possono essere effettuate nei momenti di gioco
+							fermo comunicandolo al giudice di campo.
+						</p>
+						<p className="contest-txt">
+							• Sono{" "}
+							<span className="day-txt-bolder"> tutti ammessi a giocare </span>.
+						</p>
+						<p className="contest-txt">
+							• Per la squadra vincitrice è previsto{" "}
+							<span className="day-txt-bolder"> un premio a sorpresa </span>.
+							Così come per la squadra che si classifica seconda
+						</p>
+						<p className="contest-txt">
+							• Termine adesioni : sabato 23 settembre 2023
+						</p>
+						<p className="contest-txt">
+							• Il torneo si terrà domenica 24 a partire dalle 15:00
+						</p>
+						<p className="contest-txt">
+							• Il Torneo si terrà con un minimo di 8 squadre e un massimo di 16
+							a eliminazione diretta
+						</p>
+						<p className="contest-txt">• Il torneo sarà fatto a sorteggio.</p>
+						<p className="contest-txt">
+							• Il{" "}
+							<span className="day-txt-bolder">
+								{" "}
+								costo dell’adesione è di 10€ per squadra{" "}
+							</span>{" "}
+							3 persone
+						</p>
+						<br />
+						<p className="day-txt-bold">Contest 3 punti</p>
+						<p className="contest-txt">
+							Vince la gara chi{" "}
+							<span className="day-txt-bolder">in 90 secondi di tempo</span>{" "}
+							riesce a mettere a segno il maggior numero di canestri possibile.
+							I lanci potranno essere effettuati da 3 punti diversi del campo,
+							situati nel punto più lontano del canestro.
+						</p>
+						<p className="contest-txt">
+							• Sono{" "}
+							<span className="day-txt-bolder"> tutti ammessi a giocare </span>.
+						</p>
+						<p className="contest-txt">
+							• Per la squadra vincitrice è previsto{" "}
+							<span className="day-txt-bolder"> un premio a sorpresa </span>.
+							Così come per la squadra che si classifica seconda.
+						</p>
+						<p className="contest-txt">
+							• Termine adesioni: domenica 24 settembre 2023
+						</p>
+						<br />
+						<p className="day-txt-bold">BEER MILE</p>
+						<p className="contest-txt">
+							É una corsa non competitiva{" "}
+							<span className="day-txt-bolder"> aperta a tutti </span>. Si
+							svolge sulla distanza di 1,6 km ovvero un miglio. Il percorso è
+							ottenuto correndo per 4 volte una pista di 400 metri. La gara
+							inizia sulla linea di partenza con il consumo di una birra poi
+							percorrendo un giro completo attorno al tracciato. Va ripetuto per
+							4 volte in modo simile: bere la birra e poi corsa per 400 metri.
+							Dopo il completamento del quarto giro in corsa (e dopo quattro
+							birre bevute), un concorrente ha terminato la gara dimostrando di
+							aver bevuto la birra capovolgendo il bicchiere sulla propria
+							testa.
+						</p>
+						<p className="contest-txt">
+							Si partirà a{" "}
+							<span className="day-txt-bolder">gruppi 4/6 persone</span>.
+						</p>
+						<p className="contest-txt">
+							• Per il vincitore è previsto{" "}
+							<span className="day-txt-bolder"> un premio a sorpresa </span>.
+						</p>
+						<p className="contest-txt">
+							• Saranno premiati i primi 3 classificati
+						</p>
+						<p className="contest-txt">
+							• Termine adesioni : sabato 23 settembre 2023
+						</p>
+						<p className="contest-txt">
+							• La corsa si terrà sabato 23 a partire dalle 19:00
+						</p>
+						<p className="contest-txt">
+							• Il
+							<span className="day-txt-bolder">
+								{" "}
+								costo dell’adesione è di 5€{" "}
+							</span>
+							.
+						</p>
 					</div>
 				</div>
 			</main>
